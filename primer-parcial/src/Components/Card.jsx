9ed9b54cb26d932c2790ada478967ce993cc0fbe
rocioleeg <rociogiovannalee@gmail.com>
2023-03-08 19:37:10 -0300

    segundo commit

--- primer-parcial/src/Components/Card.jsx
+++ primer-parcial/src/Components/Card.jsx
@@ -1,5 +1,9 @@
-const Card = () => {
-  return <div>Hola soy una Card</div>;
+import styles from "./Card.module.css";
+
+const Card = ({name, comidaFav}) => {
+  return <div className={styles.container}>
+    Hola {name}! <br></br> 
+  Sabemos que tu comida preferida es {comidaFav}</div>;
 };
 
 export default Card;
--- primer-parcial/src/Components/Form.jsx
+++ primer-parcial/src/Components/Form.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { useState } from "react";
+import Card from "./Card";
+function Form() {
+  const [name, setName] = useState("");
+  const [comidaFav, setComidaFav] = useState("");
+  const [visible, setVisible]=useState(false);
 
-const Form = () => {
-  return <></>;
-};
+  const validateName = (name) => {
+    if (name.length >= 3) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const validateComidaFav = (comidaFav) => {
+    if (comidaFav.length >= 6) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  function signUser(e) {
+    e.preventDefault();
+
+    if (!validateName(name) || !validateComidaFav(comidaFav)) {
+      setVisible(false);
+      alert("Por favor chequea que la información sea correcta");
+      
+     
+    }else{ setVisible(true)}
+   
+  }
+
+  const cambioNombre = (e) => {
+    setName(e.target.value);
+  };
+  const cambioComidaFav = (e) => {
+    setComidaFav(e.target.value);
+  };
+
+  return (
+    <div >
+      <h1>Ingresa tu comida preferida</h1>
+      <form onSubmit={signUser}>
+        <div>
+          <input
+            type="text"
+            placeholder="Ingrese su nombre"
+            value={name}
+            onChange={cambioNombre}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Ingrese su comida preferida"
+            value={comidaFav}
+            onChange={cambioComidaFav}
+          />
+        </div>
+        <div>
+          <button type="submit">
+            ENVIAR
+          </button>
+        </div>
+     
+      </form>
+      {visible?<Card name={name} comidaFav={comidaFav}/>:null}
+    </div>
+  );
+}
 
 export default Form;
+
